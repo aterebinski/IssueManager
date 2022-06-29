9a95c4ec68bb95d75ec34953a59fed16a7eb6d2b
Adam Terebiński <aterebinski@wp.pl>
2022-06-29 10:40:21 +0200

    Praliminary adding Job Controller

---
+++ IssueManager/Models/JobsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using IssueManager.Enums;
+using System;
+
+namespace IssueManager.Models
+{
+    public class JobsViewModel
+    {
+        public int Id { get; set; }
+        public int EntityId { get; set; }
+        public string EntityName { get; set; }
+        public int AssignmentId { get; set; }
+        public string AssignmentName { get; set; }
+        public string Note { get; set; }
+        public bool Del { get; set; }
+        public JobStatus Status { get; set; }
+        public bool Checked { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public string CreateUserId { get; set; }
+        public DateTime ModifyDateTime { get; set; }
+        public string ModifyUserId { get; set; }
+        public int HistoryPreviousId { get; set; }
+        public int HistoryNextId { get; set; }
+    }
+}
